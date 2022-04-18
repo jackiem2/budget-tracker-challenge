86367d0a0c24c50c3cd33aeaa0918a7af7567db7
jackiem2 <jacqueline.merlan@gmail.com>
2022-04-17 22:14:55 -0500

    created offline db to store info while user is offline

---
+++ public/js/db.js
@@ -0,0 +1,57 @@
+let db;
+const request = indexedDB.open("budget_tracker", 1);
+
+request.onupgradeneeded = (event) => {
+  event.target.result.createObjectStore("pending", {
+    keyPath: "id",
+    autoIncrement: true
+  });
+};
+
+request.onerror = (err) => {
+  console.log(err.message);
+};
+
+request.onsuccess = (event) => {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+// This function will be used when there is no internet connection
+function saveRecord(record) {
+  const transaction = db.transaction("pending", "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
+
+// function will be used when user goes online to send transactions stored offline
+function uploadDatabase() {
+  const transaction = db.transaction("pending", "readonly");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction("pending", "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", uploadDatabase);
